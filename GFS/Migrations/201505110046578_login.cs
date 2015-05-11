fb93a4053c367d6c2498635ec3db2ea39a80f48a
shamslam845 <shamslam845@gmail.com>
2015-05-10 17:54:50 -0700

    Fixed the login

---
+++ GFS/Migrations/201505110046578_login.cs
@@ -0,0 +1,16 @@
+namespace GFS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class login : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
